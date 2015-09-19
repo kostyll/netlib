8e4dcac8e5c4247bd1c39ba0f058d25924c0d2cf
Thomas Kriechbaumer <thomas@kriechbaumer.name>
2015-09-19 11:10:15 +0200

    migrate to hyperframe

--- netlib/http/http2/__init__.py
+++ netlib/http/http2/__init__.py
@@ -5,16 +5,10 @@ from .connections import (
     assemble_request_headers, assemble_response_headers,
     read_nonmultiplexed_request, read_nonmultiplexed_response,
 )
-from .frame import (
-    Frame,
-    HeadersFrame, DataFrame, SettingsFrame, WindowUpdateFrame
-)
 
 __all__ = [
     "Http2ClientConnection", "Http2ServerConnection",
     "make_request", "make_response",
     "assemble_request_headers", "assemble_response_headers",
     "read_nonmultiplexed_request", "read_nonmultiplexed_response",
-    "Frame",
-    "HeadersFrame", "DataFrame", "SettingsFrame", "WindowUpdateFrame"
 ]
